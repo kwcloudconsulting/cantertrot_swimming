2ffa699eb3c23dfe1794bbd6cdcc8ec669c53432
Karimarie Wilson <kwcloudconsulting@gmail.com>
2023-04-28 16:51:29 -0500

    adding graphql foundations

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,10 +7,23 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
 module.exports = {
+  /* Your site config here */
+  siteMetadata: {
+    title: "Cantertrot Swimming",
+    description: "A website for Cantertrot Swimming and private swim lessons",
+    author: "kwcloudconsulting",
+    person: { name: "john", age: 32 },
+    simpleData: ["item 1", "item 2"],
+    complexData: [
+      { name: "john", age: 32 },
+      { name: "susan", age: 21 },
+    ],
+  },
   plugins: [
     `gatsby-plugin-styled-components`,
-    "gatsby-plugin-image",
+    `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
   ],
